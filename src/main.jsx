6c86b2db65655aab41177208896a92c05aa746ae
3782291211 <Laklouk98@hotmail.com>
2023-02-09 07:34:23 +0000

    Adding browser router and provider function to main component

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter } from 'react-router-dom';
+import { GameControlsProvider } from './contexts/GameControlsContext';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <GameControlsProvider>
+     <App />
+    </GameControlsProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
