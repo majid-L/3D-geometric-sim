c6fd8b6fa43ca5527030eab562ea3e4354dbc3cd
3782291211 <Laklouk98@hotmail.com>
2023-02-09 10:39:02 +0000

    Adding component for 2D game grid

---
+++ src/components/TwoDimensionalBoard.jsx
@@ -0,0 +1,32 @@
+import { GameControlsContext } from "../contexts/GameControlsContext";
+import GameControls from "./GameControls";
+import { v4 as uuidv4 } from "uuid";
+import { useContext } from "react";
+
+const TwoDimensionalBoard = () => {
+const { gameParameters: {isRunning, configuration, interact}, setGameParameters } = useContext(GameControlsContext);
+
+const gridcolumns = "1fr ".repeat(configuration.length);
+
+return (<>
+<h1 className="tutorial_h1">Game Of Life in 2D</h1>
+    <div className="cellboard" style={{ gridTemplateColumns: gridcolumns }}>
+      {configuration.map((row, i) => {
+        return row.map((cell, k) => {
+          return <div key={uuidv4()} className={configuration[i][k] === 1 ? "cellgridliving" : "cellgriddead"} onClick={() => {
+            if (interact === true && !isRunning) {
+              setGameParameters(prev => {
+                const newConfig = structuredClone(prev.configuration);
+                newConfig[position[0]][position[2]] = living ? 0 : 1;
+                return {...prev, configuration: newConfig};
+              })
+            };
+        } }/>
+        });
+      })}
+    </div>
+    <GameControls />
+</>);
+};
+
+export default TwoDimensionalBoard;
