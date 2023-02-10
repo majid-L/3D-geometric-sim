f9a29217beb163753a95ecd2604eedfa1a31f5b7
3782291211 <Laklouk98@hotmail.com>
2023-02-10 02:48:51 +0000

    Adding axios data fetching functions

---
+++ src/api.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const getPatterns = () => {
+  return axios.get("https://automatrixapi.pythonanywhere.com/api/patterns");
+};
+
+export const getUsers = () => {
+  return axios.get("https://automatrixapi.pythonanywhere.com/api/users");
+};
+
+export const getPatternsByUser = username => {
+  return axios.get(`https://automatrixapi.pythonanywhere.com/api/users/${username}/patterns`)
+};
+
+export const postPattern = (username, pattern_name, pattern_body) => {
+  return axios.post(`https://automatrixapi.pythonanywhere.com/api/patterns`, {
+    username,
+    pattern_name,
+    pattern_body
+  })
+};
+
+export const deletePattern = id => {
+  return axios.delete(`https://automatrixapi.pythonanywhere.com/api/patterns/${id}`);
+};
+export const postUser = (user, username, email, avatar_url) => {
+  return axios.post("https://automatrixapi.pythonanywhere.com/api/users", {
+    account_owner: user,
+    username: username,
+    email: email,
+    avatar_url: avatar_url
+  });
+}
