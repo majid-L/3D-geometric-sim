cd07a084a2d2bafbecc7b6674e2c2ab28646470f
3782291211 <Laklouk98@hotmail.com>
2023-02-10 02:49:16 +0000

    Adding modal and alert components

--- src/components/GameControls.jsx
+++ src/components/GameControls.jsx
@@ -1,11 +1,18 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import { GameControlsContext } from '../contexts/GameControlsContext';
-import {SizeIcon, SpeedIcon} from './Icons';
+import {SmallIcon, LargeIcon, SpeedIcon} from './Icons';
+import { useLocation } from 'react-router-dom';
+
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import Alert from 'react-bootstrap/Alert';
+import { postPattern } from '../api';
 
 const InteractTooltip = (
     <Tooltip id="tooltip">
@@ -16,7 +23,20 @@ const InteractTooltip = (
 
 const GameControls = () => {
 
-const { controls, setControls, gameParameters: {isRunning} } = useContext(GameControlsContext);
+const [patternName, setPatternName] = useState('');
+const [show, setShow] = useState(false);
+const [showAlert, setShowAlert] = useState(false);
+const [alertMsg, setAlertMsg] = useState('');
+
+const [showSuccess, setShowSuccess] = useState(false);
+const location = useLocation();
+
+const { controls, setControls, gameParameters: {isRunning, configuration, username} } = useContext(GameControlsContext);
+
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+
+const patternBody = configuration.map(m => m.join("")).join(" ");
 
 const handleClick = e => {
   let click = 0;
@@ -31,19 +51,76 @@ const handleClick = e => {
   }
 };
 
+const handleSubmit = e => {
+  setShow(false);
+  if (!username) {
+    setAlertMsg("You must be logged in to save a pattern to your collection.");
+    setShowAlert(true);
+    return;
+  };
+  
+  if (/^([^1])+$/.test(patternBody)){
+    setAlertMsg("Pattern cannot be empty.");
+    setShowAlert(true);
+    return;
+  };
+
+  if (!patternName) {
+    setAlertMsg("Please provide a name for your pattern.");
+    setShowAlert(true);
+    return;
+  };
+ 
+  postPattern(username, patternName, patternBody).then(() => {
+    setShowSuccess(true);
+  }).catch(err => {
+    setAlertMsg(err.response.data.msg);
+    setShowAlert(true);
+  })
+}
+
 const condition = controls.button === "enablePhysics";
 
-return (
+return (<>
+<section className="game-alerts">
+  {showAlert && <Alert className="alert" variant="danger" onClose={() => setShowAlert(false)} dismissible>
+      <Alert.Heading>Something went wrong...</Alert.Heading>
+      <p>{alertMsg}</p>
+    </Alert>}
+
+    <Alert show={showSuccess} variant="success">
+        <Alert.Heading>You pattern has been added to your collection.</Alert.Heading>
+        <p>
+          Job done. Now let's get back to the game.
+        </p>
+        <Button onClick={() => setShowSuccess(false)} variant="outline-success">
+            Dismiss.
+          </Button>
+      </Alert>
+      </section>
 <section className="game-controls">
+      {location.pathname[1] === "3" && <Dropdown onClick={handleClick}>
+        <Dropdown.Toggle id="effects" variant="secondary">
+          Effects
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu variant="dark">
+          <Dropdown.Item id="stars">Stars</Dropdown.Item>
+          <Dropdown.Item id="sky">Sky</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>}
+
 <ButtonGroup onClick={handleClick} aria-label="Basic example">
   <Button disabled={condition} id="start" variant="secondary">Start</Button>
   <Button disabled={condition} id="stop" variant="secondary">Stop</Button>
   <Button disabled={condition} id="reset" variant="secondary">Reset</Button>
-  <Button disabled={condition} id="faster" variant="secondary"><SpeedIcon/> Faster</Button>
-  <Button disabled={condition} id="slower" variant="secondary">Slower</Button>
+  <Button disabled={condition} id="randomise" variant="secondary">Randomise</Button>
+  <Button disabled={condition} id="clear" variant="secondary">Clear</Button>
+  <Button disabled={condition} id="faster" variant="secondary"><SpeedIcon/>Faster</Button>
+  <Button disabled={condition} id="slower" variant="secondary"><SpeedIcon/>Slower</Button>
 
-  <Button disabled={condition || isRunning} id="larger" variant="secondary"><SizeIcon/>Larger</Button>
-  <Button disabled={condition || isRunning} id="smaller" variant="secondary">Smaller</Button>
+  <Button disabled={condition || isRunning} id="larger" variant="secondary"><LargeIcon/>Larger</Button>
+  <Button disabled={condition || isRunning} id="smaller" variant="secondary"><SmallIcon/>Smaller</Button>
 
   <DropdownButton disabled={condition} as={ButtonGroup} title="3D/2D" className="bg-nested-dropdown">
    <Dropdown.Item id="threeD" eventKey="1">3D</Dropdown.Item>
@@ -66,11 +143,43 @@ return (
    <Dropdown.Item id="enablePhysics" eventKey="1">Enable</Dropdown.Item>
    <Dropdown.Item id="disablePhysics" eventKey="2">Disable</Dropdown.Item>
  </DropdownButton>
- <Button variant="primary" id="save" onClick={() => {}}>
+ <Button variant="primary" id="save" onClick={handleShow}>
         Save pattern
       </Button>
 </ButtonGroup>
-</section>)
+
+<Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Save pattern</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>What would you like to name this pattern?</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter pattern name."
+                autoFocus
+                onChange={e => setPatternName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSubmit}>
+            Save pattern
+          </Button>
+        </Modal.Footer>
+      </Modal>
+</section></>)
 };
 
 export default GameControls;
