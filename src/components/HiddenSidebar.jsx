3470f12b535e803148e9ef6afba52b30208d3c82
3782291211 <Laklouk98@hotmail.com>
2023-02-11 23:36:19 +0000

    Updating tips section

--- src/components/HiddenSidebar.jsx
+++ src/components/HiddenSidebar.jsx
@@ -10,7 +10,11 @@ function HiddenSidebar({showSidebar, setShowSidebar}) {
           <Offcanvas.Title>Tips and tricks</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          Multiply...or die. Is it a game or is it reality? Are you an automaton or the game master?
+          <p>Multiply...or die. Is it a game or is it reality? Are you an automaton or the game master?
+          If you find that your patterns keeps getting stuck at the edges, you may need to enable the edge wrap feature. This allows the pattern to completely ignore all edges, effectively creating an infinite play area and allowing your patern to disappear from one side and reappear at the other.</p>
+
+          <p>There may be times when you may wish to disable the edge wrap feature. This is especially important if you are using the Gosper glider gun pattern, which requires a hard edge to achieve its full potential.</p>
+          
         </Offcanvas.Body>
       </Offcanvas>
     </>
