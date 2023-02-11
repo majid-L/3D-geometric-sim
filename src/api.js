d9bf6987c16264d7f6aa54c749035bd93d48e1ed
3782291211 <Laklouk98@hotmail.com>
2023-02-11 05:32:27 +0000

    Adding login and signup API functions

--- src/api.js
+++ src/api.js
@@ -23,6 +23,7 @@ export const postPattern = (username, pattern_name, pattern_body) => {
 export const deletePattern = id => {
   return axios.delete(`https://automatrixapi.pythonanywhere.com/api/patterns/${id}`);
 };
+
 export const postUser = (user, username, email, avatar_url) => {
   return axios.post("https://automatrixapi.pythonanywhere.com/api/users", {
     account_owner: user,
@@ -30,4 +31,12 @@ export const postUser = (user, username, email, avatar_url) => {
     email: email,
     avatar_url: avatar_url
   });
-}
+};
+
+export const login = (username, password) => {
+  return axios.post('https://automatrixapi.pythonanywhere.com/mongo_auth/login/', {username, password});
+};
+
+export const signup = (username, email, password) => {
+  return axios.post('https://automatrixapi.pythonanywhere.com/mongo_auth/signup/', {username, email, password});
+};
