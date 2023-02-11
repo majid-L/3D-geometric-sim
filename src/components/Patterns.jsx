fad016af82825cf9d71afa0637d266017da4af67
3782291211 <Laklouk98@hotmail.com>
2023-02-11 20:23:56 +0000

    Adding loading and error states

--- src/components/Patterns.jsx
+++ src/components/Patterns.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 import { getPatterns, getUsers } from "../api";
 import PatternCard from './PatternCard';
+import { Loading } from "./Loading";
+import Alert from 'react-bootstrap/Alert';
 
 function Patterns() {
   const [patternsData, setPatternsData] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [alertMsg, setAlertMsg] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     Promise.all([getPatterns(), getUsers()])
     .then(([{data: {patterns}}, {data: {users}}]) => {
       const patternsData = patterns.map(pattern => {
@@ -17,70 +23,30 @@ function Patterns() {
         return pattern;
       })
       setPatternsData(patternsData);
-    });
+    }).catch(({response: {data}}) => {
+      if (data.msg) {
+        setAlertMsg(data.msg);
+        setShowAlert(true);
+      };
+    }).finally(() => {
+      setIsLoading(false);
+    })
   }, []);
 
   return (<main>
     <h1 className="patterns_h1">Patterns</h1>
-    <section className="patterns_list">
+
+      {showAlert && <Alert style={{width: '80%', maxWidth: '820px'}} id="profile_alert" variant="danger" onClose={() => setShowAlert(false)} dismissible>
+        <Alert.Heading>Something went wrong.</Alert.Heading>
+        <p style={{margin: 0}}>{alertMsg}</p>
+      </Alert>}
+
+    {isLoading ? <Loading/> : !alertMsg ? <section className="patterns_list">
       {patternsData && patternsData.map(pattern => {
         return <PatternCard key={pattern._id} id={pattern._id} username={pattern.username} pattern_name={pattern.pattern_name} avatar_url={pattern.avatar_url} created_at={pattern.created_at} pattern_body={pattern.pattern_body} setPatternsData={setPatternsData}/>
       })}
-    </section>
+    </section> : null}
   </main>)
 
 }
 export default Patterns;
-
-
-
-/*
-
-return  <Card key={pattern._id} style={{ width: '21rem' }}>
-        <Card.Header>
-            <Nav variant="pills" defaultActiveKey="#first">
-              <Nav.Item>
-                <Nav.Link href="#first">Go 3D</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="#link">Go 2D</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="#">
-                  More patterns
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Card.Header>
-          <Card.Img variant="top" src={pattern.avatar_url} />
-          <Card.Body>
-            <Card.Title>"{pattern.pattern_name}"</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">By {pattern.username}</Card.Subtitle>
-            <Card.Text>
-              Added on {pattern.created_at.slice(0, 10)}
-            </Card.Text>
-            <div className="grid" 
-            style={{
-              'display': "grid", 
-              'gridTemplateColumns': `repeat(${pattern.pattern_body.split(" ")[0].length}, 25px)`,
-              'boxShadow': "6px 10px 17px" }}>
-           {stringToArray(pattern.pattern_body).map((row, i) => {
-            return row.map((col, k) => (
-             <div className="cell"
-              key={`${i}-${k}`}
-              style={{
-                'width': '100%',
-                'height': '25px',
-                'backgroundColor': stringToArray(pattern.pattern_body)[i][k] ? "pink" : '',
-                'boxShadow': stringToArray(pattern.pattern_body)[i][k] ? "0px 0 6px inset" : "",
-                'border': "solid 1px black",
-              }}
-             />
-             ))
-            }
-            )}
-            </div>
-            <Button className="pattern_delete" variant="primary">Delete</Button>
-          </Card.Body>
-        </Card>
-*/
