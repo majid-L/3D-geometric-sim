0e74fc26c0ffafe1a7d442b6e3a4dc96149a28a0
3782291211 <Laklouk98@hotmail.com>
2023-02-11 07:21:23 +0000

    Updating user patterns component

--- src/components/UserPatterns.jsx
+++ src/components/UserPatterns.jsx
@@ -1,35 +1,35 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 import { getPatternsByUser, getUsers } from "../api";
+import { GameControlsContext } from "../contexts/GameControlsContext";
 import PatternCard from './PatternCard';
 
 function UserPatterns () {
-const [username, setUsername] = useState('zack81');
 const [userPatterns, setUserPatterns] = useState([]);
-const [userData, setUserData] = useState([]);
+const [isLoading, setIsLoading] = useState(true);
+const {gameParameters: {username: loggedInUser}} = useContext(GameControlsContext);
+const {username} = useParams();
 
 useEffect(() => {
-  Promise.all([
-    getPatternsByUser(username),
-    getUsers()
-  ]).then(([{data: {patterns}}, {data: {users}}]) => {
+  getPatternsByUser(username)
+  .then(({data: {patterns}}) => {
     setUserPatterns(patterns);
-    users.forEach(user => {
-      if(user.username === username){
-        setUserData(user);
-      }
-    })
+    setIsLoading(false);
+  })
+  .catch(err => {
+    setIsLoading(false);
+    return;
   })
 }, []);
 
-if (userPatterns.length && userData.username) {
+if (userPatterns) {
   return (<main>
-    <div className="user_title">
-    <h1 className="user_h1">Welcome back, {userData.account_owner.split(" ")[0]}.</h1>
-    <img className="user_img" src={userData.avatar_url}/></div>
+    <h1 className="user_h1">{loggedInUser === username ? `Welcome back, ${username}.` : `${username}'s patterns`}</h1>
+    {!isLoading && userPatterns.length ? <p className="user_intro">{loggedInUser === username ? 'Here are your saved patterns.' : `You are currently viewing all patterns submitted by ${username}.`}</p> : !isLoading && !userPatterns.length ? <p className="user_intro">{loggedInUser === username ? 'You haven\'t created any patterns yet.' : `${username} hasn\'t posted any patterns yet.`}</p> : null}
     <article className="patterns_list">
         {userPatterns.map(pattern => {
-          return <PatternCard key={pattern._id} username={pattern.username} pattern_name={pattern.pattern_name} avatar_url={pattern.avatar_url} created_at={pattern.created_at} pattern_body={pattern.pattern_body}/>
+          return <PatternCard key={pattern._id} id={pattern._id} username={pattern.username} pattern_name={pattern.pattern_name} avatar_url={pattern.avatar_url} created_at={pattern.created_at} pattern_body={pattern.pattern_body} setPatternsData={setUserPatterns}/>
         })}
     </article>
   </main>)
