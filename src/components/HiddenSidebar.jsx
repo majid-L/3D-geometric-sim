925538ee33a3b9fb12d3590ee5567176a47b308a
3782291211 <Laklouk98@hotmail.com>
2023-02-12 23:01:22 +0000

    Adding game tips and explanations

--- src/components/HiddenSidebar.jsx
+++ src/components/HiddenSidebar.jsx
@@ -7,14 +7,29 @@ function HiddenSidebar({showSidebar, setShowSidebar}) {
   return (
     <> <Offcanvas show={showSidebar} onHide={handleClose}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Tips and tricks</Offcanvas.Title>
+          <Offcanvas.Title style={{fontSize: '22px'}}>Tips and tricks</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          <p>Multiply...or die. Is it a game or is it reality? Are you an automaton or the game master?
-          If you find that your patterns keeps getting stuck at the edges, you may need to enable the edge wrap feature. This allows the pattern to completely ignore all edges, effectively creating an infinite play area and allowing your patern to disappear from one side and reappear at the other.</p>
+          <p>Multiply...or die. Is it a game or is it reality? Are you an automaton or the game master?</p>
 
-          <p>There may be times when you may wish to disable the edge wrap feature. This is especially important if you are using the Gosper glider gun pattern, which requires a hard edge to achieve its full potential.</p>
-          
+          <p className="tips_title">Camera controls</p>
+          <ul><li>To zoom in and out, use the provided slider rather than your mouse scroll wheel.</li>
+          <li> To rotate the camera, hold down the left mouse button and move your mouse around. (The centre of your screen is used as a fixed pivot.)</li>
+          <li>To move the camera, hold down the right moust button and move your mouse around.</li>
+          </ul>
+
+          <p className="tips_title">Effects</p>
+          <p>For the 3D board, there are several visual effects available to choose from, including block color, bloom and background scene. Make sure to give these a try!</p>
+
+          <p className="tips_title">Edge type</p>
+          <p>This controls the behaviour of the cells when they meet an edge. This has a significant impact on the way the game plays out.</p>
+          <ul>
+            <li>Hard edge: opposite edges are completely ignored. When a block is at the very edge of the board, it is considered to have 5 neighbours as opposed to the usual 8.</li>
+            <li>Wrap around: your pattern will completely ignore all edges, such that when a block is at the very edge of the board, it does not meet a hard stop. Instead, blocks at the immediate start of the opposite edge are counted as being neighbours, so the cell still has 8 neightbours (5 immediate neighbours plus 3 neighbours at the opposing edge). This effectively creates an infinite play area and allows your pattern to disappear from one side and reappear at the other.</li>
+          </ul>
+
+          <p>You'll almost always want to keep this set to "Wrap around". However, there may be times when you may wish to set it to "Hard edge". For instance, the <span className="tips_span">Gosper glider gun</span> pattern requires a hard edge to achieve its full potential. Play around with this feature to see if you can find any other good uses for it!</p>
+
         </Offcanvas.Body>
       </Offcanvas>
     </>
