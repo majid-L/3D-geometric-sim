1f6ff38ff727c405193d3b07564cef278889d684
3782291211 <Laklouk98@hotmail.com>
2023-02-12 23:43:45 +0000

    Adding icon to 404 page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,3 +1,8 @@
-export const NotFound = () => {
+import { Emoticon } from "./Icons";
 
+export const NotFound = () => {
+return (<>
+<h2 style={{margin: '200px 0 80px'}} className="profile_h2">Page not found.</h2>
+<Emoticon/>
+</> );
 };
