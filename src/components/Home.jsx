e118ec76bca507aaad7b1c60332ce78bff60068c
3782291211 <Laklouk98@hotmail.com>
2023-02-13 07:45:26 +0000

    Adding new effects and components to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,80 +1,114 @@
-import {
-    CameraControls,
-    Center,
-    Environment,
-    Float,
-    OrbitControls,
-    PerspectiveCamera,
-    Text3D,
-    useMatcapTexture,
-  } from "@react-three/drei";
-  import { Canvas, useThree } from "@react-three/fiber";
-  import retro from "../assets/retro.json";
-  import { CubeTextureLoader } from "three";
-  import { Bloom, EffectComposer, GodRays } from "@react-three/postprocessing";
+import {CameraControls, PerspectiveCamera} from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Text } from "./Text";
+import { EffectComposer, GodRays, Glitch, Noise } from "@react-three/postprocessing";
+import { forwardRef, Suspense, useMemo, useRef } from "react";
+import { BlendFunction, Resizer, KernelSize, GlitchMode } from "postprocessing";
+import * as THREE from "three";
+import { useResource } from "react-three-fiber";
+import { apply as applySpring, useSpring, a, interpolate } from 'react-spring/three';
+
+
+const Sun = forwardRef(function Sun(props, forwardRef) {
+  useFrame(({ clock }) => {
+    forwardRef.current.position.x = Math.sin(clock.getElapsedTime()) * -8;
+    forwardRef.current.position.y = Math.cos(clock.getElapsedTime()) * -8;
+  });
+
+  return (
+    <mesh ref={forwardRef} position={[0, 0, -15]}>
+      <sphereGeometry args={[1, 36, 36]} />
+      <meshBasicMaterial color={"#C58D47"} />
+    </mesh>
+  );
+});
+
+function Effects() {
+  const sunRef = useResource();
+  return (
+    <>
+      <Sun ref={sunRef} />
+      {sunRef.current && (
+        <EffectComposer multisampling={0}>
+          <GodRays
+            sun={sunRef.current}
+            blendFunction={BlendFunction.Screen}
+            samples={30}
+            density={0.97}
+            decay={0.96}
+            weight={0.6}
+            exposure={0.4}
+            clampMax={1}
+            width={Resizer.AUTO_SIZE}
+            height={Resizer.AUTO_SIZE}
+            kernelSize={KernelSize.SMALL}
+            blur={true}
+          />
+            <Glitch
+            delay={[1.5, 7.5]} // min and max glitch delay
+            duration={[0.6, 1.0]} // min and max glitch duration
+            strength={[0.3, 1.0]} // min and max glitch strength
+            mode={GlitchMode.SPORADIC} // glitch mode
+            active // turn on/off the effect (switches between "mode" prop and GlitchMode.DISABLED)
+            ratio={0.85} // Threshold for strong glitches, 0 - no weak glitches, 1 - no strong glitches.
+            />
+            <Noise
+            premultiply // enables or disables noise premultiplication
+            blendFunction={BlendFunction.ADD} // blend mode
+            />
+        </EffectComposer>
+      )}
+    </>
+  );
+};
+
+
+function Stars({ position }) {
+  let group = useRef();
+  let theta = 0;
+  useFrame(() => {
+    const r = 5 * Math.sin(THREE.MathUtils.degToRad((theta += 0.01)));
+    const s = Math.cos(THREE.MathUtils.degToRad(theta * 2));
+    group.current.rotation.set(r, r, r);
+    group.current.scale.set(s, s, s);
+  });
+  const [geo, mat, coords] = useMemo(() => {
+    const geo = new THREE.SphereGeometry(1, 10, 10);
+    const mat = new THREE.MeshBasicMaterial({ color: new THREE.Color('peachpuff'), transparent: true });
+    const coords = new Array(1000).fill().map(i => [Math.random() * 800 - 400, Math.random() * 800 - 400, Math.random() * 800 - 400]);
+    return [geo, mat, coords];
+  }, []);
   
-  const Home = () => {
-    function MatCap({ texture }) {
-      const [matcap] = useMatcapTexture(texture, 256);
-      return <meshMatcapMaterial matcap={matcap} />;
-    };
-  
-    return (
-      <section className="anim">
-        <Canvas>
-          <EffectComposer>
-            <Bloom mipmapBlur luminanceThreshold={1}></Bloom>
-            
-          </EffectComposer>
-          <PerspectiveCamera makeDefault fov={75} position={[10, 10, 10]} />
-          <CameraControls />
-  
-          <ambientLight intensity={0.8} />
-          <Float
-            speed={2.4}
-            rotationIntensity={2.4}
-            floatIntensity={2}
-            floatingRange={[-2, 2]}
-          >
-            <Center>
-              <Text3D
-                font={retro}
-                height={0.5}
-                letterSpacing={-0.06}
-                size={2.8}
-                curveSegments={10}
-                bevelEnabled
-                bevelThickness={0.02}
-                bevelOffset={0}
-                bevelSegments={5}
-              >
-                {" "}
-                AUTOMATRIX
-                <MatCap texture={"422509_C89536_824512_0A0604"} />{" "}
-              </Text3D>
-  
-              <mesh position={[2, -4, 0]}>
-                <Text3D
-                  font={retro}
-                  height={0.5}
-                  letterSpacing={-0.06}
-                  size={2}
-                  curveSegments={10}
-                  bevelEnabled
-                  bevelThickness={0.02}
-                  bevelOffset={0}
-                  bevelSegments={5}
-                >
-                  {" "}
-                  Multiply or die.
-                  <MatCap texture={"3B3C3F_DAD9D5_929290_ABACA8"} />{" "}
-                </Text3D>
-              </mesh>
-            </Center>
-          </Float>
-        </Canvas>
-      </section>
-    );
-  };
-  
-  export default Home;
+  return (
+    <a.group ref={group} position={position}>
+      {coords.map(([p1, p2, p3], i) => (
+        <mesh key={i} geometry={geo} material={mat} position={[p1, p2, p3]} />
+      ))}
+    </a.group>
+  )
+};
+
+const Home = () => {
+ const [{ top }] = useSpring(() => ({ top: 0}));
+
+return (
+<section className="anim">
+  <Canvas
+    camera={{ position: [0, 0, 10] }}
+    onCreated={({ gl }) => {
+      gl.setClearColor(new THREE.Color("#000000"));
+    }}>
+    <PerspectiveCamera makeDefault fov={75} position={[0, 0, 15]} />
+    <CameraControls />
+    <ambientLight intensity={0.8} />
+    <pointLight position={[15, 15, 15]} intensity={1} />
+    <Suspense fallback={null}>
+    </Suspense>
+    <Effects />
+    <Stars position={top.interpolate(top => [0, -1 + top / 20, 0])} />
+    <Text/>
+  </Canvas>
+</section>);
+};
+
+export default Home;
