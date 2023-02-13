a5896afa806dd6618d76fbc590214e05260ba57e
3782291211 <Laklouk98@hotmail.com>
2023-02-13 18:16:20 +0000

    Updating states and inline styles

--- src/components/TwoDimensionalBoard.jsx
+++ src/components/TwoDimensionalBoard.jsx
@@ -1,23 +1,23 @@
 import { GameControlsContext } from "../contexts/GameControlsContext";
-import GameControls from "./GameControls";
 import { v4 as uuidv4 } from "uuid";
 import { useContext } from "react";
 import { Controls } from "./Controls";
+import NewPattern from "./NewPattern";
 
 const TwoDimensionalBoard = () => {
-const { gameParameters: {isRunning, configuration, interact, emissive, edgeColor}, setGameParameters } = useContext(GameControlsContext);
+const { gameParameters: {configuration, interact, cellColor, boxShadow, background}, setGameParameters } = useContext(GameControlsContext);
 
 const gridcolumns = "1fr ".repeat(configuration.length);
 
 return (<>
 <h1 style={{marginTop: '70px'}} className="tutorial_h1">Game Of Life in 2D</h1>
-    <div className="cellboard" style={{ gridTemplateColumns: gridcolumns }}>
+    <div className="cellboard" style={{ gridTemplateColumns: gridcolumns, backgroundColor: background }}>
       {configuration.map((row, i) => {
         return row.map((cell, k) => {
           return <div key={uuidv4()} 
-          style={configuration[i][k] ? {backgroundColor: emissive, boxShadow: `0 0 6px 6px inset ${edgeColor}`} : {}}
+          style={configuration[i][k] ? {backgroundColor: cellColor, boxShadow: `0 0 6px 6px inset ${boxShadow}`} : {}}
           className={configuration[i][k] === 1 ? "cellgridliving" : "cellgriddead"} onClick={() => {
-            if (interact === true && !isRunning) {
+            if (interact === true) {
               setGameParameters(prev => {
                 const newConfig = structuredClone(configuration);
                 newConfig[i][k] = configuration[i][k] ? 0 : 1;
@@ -28,7 +28,7 @@ return (<>
         });
       })}
     </div>
-    <GameControls />
+    <NewPattern />
     <Controls/>
 </>);
 };
