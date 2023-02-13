886a1572045ab59e7910b9601acee28c1c6c9e08
3782291211 <Laklouk98@hotmail.com>
2023-02-13 18:15:04 +0000

    Adding separate color palettes for 3d/2d

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -8,31 +8,33 @@ import { useLocation } from 'react-router-dom';
 
 export function Controls() {
     const {setGameParameters} = useContext(GameControlsContext);
-    const location = useLocation().pathname[1] === '3';
+    const threeDBoard = useLocation().pathname[1] === '3';
+    const twoDBoard = useLocation().pathname[1] === '2';
 
-    const { zoom } = location && useControls({ zoom: { value: 0.8, min: 0, max: 2.5, step: 0.01 } });
+    const { zoom } = threeDBoard && useControls({ zoom: { value: 0.8, min: 0, max: 2.5, step: 0.01 } });
 
-    const { "bloom %": bloomIntensity } = location && useControls({ "bloom %": { value: 1.5, min: 0, max: 5.5, step: 0.01 } });
+    const { "bloom %": bloomIntensity } = threeDBoard && useControls({ "bloom %": { value: 1.5, min: 0, max: 5.5, step: 0.01 } });
 
     const { interval } = useControls({ interval: { value: 600, min: 0, max: 1500, step: 1 } });
     const { wrap } = useControls({ wrap : true});
-    const { color } = useControls({color : 'hotpink'});
-    const { "edges": edgeColor } = useControls({"edges" : 'grey'});
+    const { "color": emissive } = threeDBoard && useControls({"color" : 'hotpink'});
+    const {"color": cellColor} = twoDBoard && useControls({"color": 'rgb(250, 162, 55)'});
+    const { "edges": edgeColor } = threeDBoard && useControls({"edges" : 'grey'});
+
+    const {"box shadow": boxShadow} = twoDBoard && useControls({"box shadow": 'rgb(187, 100, 1)'});
     
-    const { "3D text" : text} = location && useControls({"3D text": true});
+    const { "3D text" : text} = threeDBoard && useControls({"3D text": true});
     const {interact} = useControls({interact: true});
 
-    const {physics} = location && useControls({physics: false});
+    const {physics} = threeDBoard && useControls({physics: false});
 
-    useControls({ "save pattern" : button(() => {
-       
-      })});
+    const {background} = twoDBoard && useControls({background: 'rgb(22, 19, 15)'});
     
-    location && useFrame(state => {
+    threeDBoard && useFrame(state => {
       state.camera.zoom = THREE.MathUtils.lerp(state.camera.zoom, zoom * 5.3, 0.4);
       state.camera.updateProjectionMatrix();
     });
-    
+        
     useEffect(() => {
       setGameParameters(prev => ({...prev, bloomIntensity}));
     }, [bloomIntensity]);
@@ -50,6 +52,10 @@ export function Controls() {
     }, [edgeColor]);
 
     useEffect(() => {
+      setGameParameters(prev => ({...prev, boxShadow }));
+    }, [boxShadow]);
+
+    useEffect(() => {
       setGameParameters(prev => ({...prev, interval}));
     }, [interval]);
 
@@ -58,11 +64,19 @@ export function Controls() {
     }, [wrap]);
 
     useEffect(() => {
-      setGameParameters(prev => ({...prev, emissive: color}));
-    }, [color]);
+      setGameParameters(prev => ({...prev, emissive}));
+    }, [emissive]);
+
+    useEffect(() => {
+        setGameParameters(prev => ({...prev, cellColor}));
+      }, [cellColor]);
     
     useEffect(() => {
       setGameParameters(prev => ({...prev, interact}));
     }, [interact]);
+
+    useEffect(() => {
+        setGameParameters(prev => ({...prev, background}))
+    }, [background]);
 
 };
