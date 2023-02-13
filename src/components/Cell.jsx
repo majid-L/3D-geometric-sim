de09b3248c9c6b7e1e837ada8712a25ea7579473
3782291211 <Laklouk98@hotmail.com>
2023-02-13 16:12:53 +0000

    Updating props

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,12 +2,15 @@ import { Edges } from "@react-three/drei"
 import { useContext, useEffect, useState } from "react";
 import { GameControlsContext } from "../contexts/GameControlsContext";
 
+const bloomIntensities = [0.7, 1.4, 2.4, 3, 4, 5.5];
+const reducedBloomIntensities = [0.4, 0.7, 1, 1.3, 1.6, 2];
+
 function Cell({ position, living, interact, setGameParameters, isRunning, bloom }) {
-  const {gameParameters : {emissive}} = useContext(GameControlsContext);
-  
+  const {gameParameters : {emissive, bloomIntensity, edgeColor}} = useContext(GameControlsContext);
+  //const {isActive, intensity} = bloom;
     return (
         <mesh onClick={() => {
-            if (interact === true && !isRunning) {
+            if (interact === true) {
               setGameParameters(prev => {
                 const newConfig = structuredClone(prev.configuration);
                 newConfig[position[0]][position[2]] = living ? 0 : 1;
@@ -16,8 +19,8 @@ function Cell({ position, living, interact, setGameParameters, isRunning, bloom 
             };
         } } position={position}>
             {living ? <>
-            <meshStandardMaterial emissive={emissive} emissiveIntensity={!bloom ? 2 : 2.6} transparent={!bloom} toneMapped={false} opacity={0.9} />
-            <Edges color="rgb(200, 60, 200)"/>
+            <meshStandardMaterial emissive={emissive} emissiveIntensity={!bloom ? 2 : bloomIntensity} transparent={!bloom} toneMapped={false} opacity={0.9} />
+            <Edges color={edgeColor}/>
             <boxGeometry/></> :
             <><mesh position={[0, -0.5, 0]}>
             <meshStandardMaterial color="pink" transparent opacity={0.6}/>
