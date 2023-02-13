f6995e460d10150d8772a41917edc43efa42fe89
3782291211 <Laklouk98@hotmail.com>
2023-02-13 02:52:39 +0000

    Adding 'about' section to sidebar

--- src/components/HiddenSidebar.jsx
+++ src/components/HiddenSidebar.jsx
@@ -12,14 +12,22 @@ function HiddenSidebar({showSidebar, setShowSidebar}) {
         <Offcanvas.Body>
           <p style={{fontStyle: 'italic'}}>"Is it a game or is it reality? Am I an automaton or am I in control? Will I multiply, or die?"</p>
 
+          <p className="tips_title">About this game</p>
+          <p>Best thought of as a simulation rather than a game, Automatrix simulates a two-dimensional cellular automaton and is the latest interpretation of John Conway's Game Of Life. When you run the simulation, what you are seeing is a series of algorithmic computations which alter the binary state of individual cells, with each round of computations transforming the configuration of the pattern that you see on the board.</p><p>It may seem like a simple thing on the surface, but this game is based on deep complexity that spreads across various fields, several of which are mathematical in nature. It is a fascinating topic that deserves an in-depth exploration, especially for those who are numerically inclined.</p>
+
+          <div style={{borderBottom: '2px solid grey', margin: '20px 0'}}/>
+
           <p className="tips_title">Switching between 3D and 2D views</p>
-          <p>The game keeps constant track of your board configuration such that you're able to freely switch between the 3D and 2D boards while the game continues to run and your pattern continues to evolve.</p>
+          <p>The game keeps constant track of your board configuration such that you're able to freely switch between the 3D and 2D boards while the game continues to make its computations and your pattern continues to evolve.</p>
 
           <p className="tips_title">Camera controls</p>
           <ul><li>To zoom in and out, use the provided slider rather than your mouse scroll wheel.</li>
           <li> To rotate the camera, hold down the left mouse button and move your mouse around. (The centre of your screen is used as a fixed pivot.)</li>
           <li>To move the camera, hold down the right moust button and move your mouse around.</li>
           </ul>
+
+          <p className="tips_title">Changing board size</p>
+          <p>To increase or decrease the size of the board, first pause the game, make the adjustment and then resume the game.</p>
 
           <p className="tips_title">Effects</p>
           <p>For the 3D board, there are several visual effects available to choose from, including block color, bloom and background scene. Make sure to give these a try!</p>
