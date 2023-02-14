319cb7011c9a6d1ce18e4a8769f481df96ec2fde
3782291211 <Laklouk98@hotmail.com>
2023-02-14 20:52:56 +0000

    Updating physics api and adding controls

--- src/components/PhysicsScene.jsx
+++ src/components/PhysicsScene.jsx
@@ -3,8 +3,12 @@ import { Physics, useBox, usePlane } from '@react-three/cannon';
 import { useFrame } from '@react-three/fiber';
 import { Center, PerspectiveCamera, Stars } from '@react-three/drei';
 import * as THREE from "three";
+import { useContext } from 'react';
+import { GameControlsContext } from '../contexts/GameControlsContext';
 
-function PhysicsScene () {
+function PhysicsScene ({bloom}) {
+
+  const {gameParameters : {emissive, bloomIntensity, edgeColor}} = useContext(GameControlsContext);
 
     const positions = [
         [0, 2, 3],
@@ -16,48 +20,41 @@ function PhysicsScene () {
       ];
 
     function Plane() {
-    const [ref, api] = usePlane(() => ({ mass: 1, position: [0, -5, 0], type: "Static", rotation : [-Math.PI / 2, 0, 0] }));
-
-    useFrame(({ mouse }) => {
-        api.rotation.set(-Math.PI / 2 - mouse.y * 0.2, 0 + mouse.x * 0.2, 0);
-      });
+    const [ref, api] = usePlane(() => ({ mass: 1, position: [0, 0, 0], type: "Static", rotation : [-Math.PI / 2, 0, 0] }));
 
     return (
-      <mesh ref={ref} scale={20} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-        <planeGeometry attach="geometry" args={[10, 10]}/>
-        <meshLambertMaterial side={THREE.DoubleSide} emissive="yellow" emissiveIntensity={5} attach="material" color="rgb(90, 100, 200)"/>
+      <mesh ref={ref} position={[100, 100, 100]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+        <planeGeometry attach="geometry" args={[30, 30]}/>
+        <meshLambertMaterial side={THREE.DoubleSide} transparent={!bloom} toneMapped={false} opacity={0.9} emissive={edgeColor} emissiveIntensity={!bloom ? 2 : bloomIntensity} attach="material" color={emissive}/>
       </mesh>
      )
   }
 
-  function Box(position) {
-    const [ref, api] = useBox(() => ({ mass: 1, position : [0, 1, 0] }));
-   // useFrame(({ clock }) => api.position.set(Math.sin(clock.getElapsedTime()) * 5, 3, 0));
-    const direction = Math.random().toString()[2];
+  function Box() {
+    const [ref, api] = useBox(() => ({ mass: 1, position : [0, 2, 0] }));
+    const vector = Math.random();
+    console.log(vector);
    return (
     <mesh 
       onClick={() => {
-      api.velocity.set(direction, direction, direction);
+      api.velocity.set(2, 5, 0);
       }} 
       ref={ref} 
-      position={position}>
-      <boxGeometry attach="geometry" args={[5, 5, 5]}/>
-      <meshLambertMaterial attach="material" color="rgb(250,50,50)" />
+      position={[0, 2, 0]}>
+      <boxGeometry attach="geometry"/>
+      <meshLambertMaterial attach="material" color={emissive} />
     </mesh>
    )
   }
 
 return (<>
- <PerspectiveCamera makeDefault fov={85} position={[25, 55, 15]}/>
+ <PerspectiveCamera makedefault fov={85} position={[7, -4, 0]}>
  <Stars/>
 <Physics>
-<Center>
+  <Box/>
   <Plane/>
-  {positions.map((position, i) => (
-          <Box position={position} key={i} />
-        ))}
-</Center>
 </Physics>
+</PerspectiveCamera>
 </>)
 
 };
