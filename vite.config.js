6b5586ddea0adc890dda5404a93f2b9f0f759f82
3782291211 <Laklouk98@hotmail.com>
2023-02-15 13:56:40 +0000

    Removing base configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/3D-geometric-sim/"
+  plugins: [react()]
 })
