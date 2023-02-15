5cec41dece548661955049cf4089a0aaa92c76e7
3782291211 <Laklouk98@hotmail.com>
2023-02-15 23:42:45 +0000

    Adding main tag

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,8 @@
 import { Emoticon } from "./Icons";
 
 export const NotFound = () => {
-return (<>
+return (<main className="not-found">
 <h2 style={{margin: '200px 0 80px'}} className="profile_h2">Page not found.</h2>
 <Emoticon/>
-</> );
+</main> );
 };
