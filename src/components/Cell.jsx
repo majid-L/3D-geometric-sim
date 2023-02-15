bfcf43ad5db613d986ceaa2bcb0d504d60d7e0ad
3782291211 <Laklouk98@hotmail.com>
2023-02-15 05:04:02 +0000

    Adding mass to blocks

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,20 +1,26 @@
+import { useBox } from "@react-three/cannon";
 import { Edges } from "@react-three/drei"
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { GameControlsContext } from "../contexts/GameControlsContext";
 
 function Cell({ position, living, interact, setGameParameters, bloom }) {
-  const {gameParameters : {emissive, bloomIntensity, edgeColor}} = useContext(GameControlsContext);
+  const {gameParameters : {emissive, bloomIntensity, edgeColor, physics}} = useContext(GameControlsContext);
+
+  const [cellRef, api] = useBox(() => ({ mass: 10, position }));
 
     return (
-        <mesh onClick={() => {
-            if (interact === true) {
-              setGameParameters(prev => {
-                const newConfig = structuredClone(prev.configuration);
-                newConfig[position[0]][position[2]] = living ? 0 : 1;
-                return {...prev, configuration: newConfig};
-              })
-            };
-        } } position={position}>
+        <mesh ref={cellRef} onClick={() => {
+         // if (living && physics) {
+           api.velocity.set(10, 30, 1);
+        //  };
+          if (interact === true) {
+            setGameParameters(prev => {
+              const newConfig = structuredClone(prev.configuration);
+              newConfig[position[0]][position[2]] = living ? 0 : 1;
+              return {...prev, configuration: newConfig};
+            })
+          };
+        }} position={position}>
             {living ? <>
             <meshStandardMaterial emissive={emissive} emissiveIntensity={!bloom ? 2 : bloomIntensity} transparent={!bloom} toneMapped={false} opacity={0.9} />
             <Edges scale={1.01} color={edgeColor}/>
