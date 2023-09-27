d638b7891af861f5462c08c0164245151265d5c1
3782291211 <Laklouk98@hotmail.com>
2023-09-27 06:37:09 +0100

    Replacing hash router

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import App from './App'
 import './index.css'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { GameControlsProvider } from './contexts/GameControlsContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <HashRouter>
+  <BrowserRouter>
     <GameControlsProvider>
      <App />
     </GameControlsProvider>
-    </HashRouter>,
+  </BrowserRouter>
 );
